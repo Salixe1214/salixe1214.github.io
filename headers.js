59d265402768e927d515b6cb30cb07c89ab0617f
ast-amand <alexis.st-amand@novolecs.com>
2026-04-15 11:18:42 -0400

    + dynamic headers

---
+++ headers.js
@@ -0,0 +1,29 @@
+let menu_els = {
+  'Acceuil': "index.html",
+  'Wordle': "aideWordle.html",
+  'Anime': "animePicker.html",
+  'Portfolio': "portfolio.html",
+  'Wood Calculator': "woodCalculator.html"
+};
+let head_el = document.getElementById("headers");
+
+dictList = (dict) => {
+  for(let item in dict) {
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    a.href = dict[item];
+    if(document.baseURI.split('/').at(-1) === dict[item]){
+      a.className = "active";
+    } else if (document.baseURI.split('/').at(-1) === "" && item === "Accueil"){
+      a.className = "active";
+    } else {
+      a.className = "menu";
+    };
+    a.textContent = item;
+    li.appendChild(a);
+    li.className = "menu"
+    head_el.appendChild(li)
+  };
+}
+
+dictList(menu_els);
