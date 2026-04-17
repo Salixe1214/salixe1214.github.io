987da01157a15f9e46cfefae27fc3e63bb0a7df3
ast-amand <alexis.st-amand@novolecs.com>
2026-04-17 09:42:11 -0400

    + game2

--- headers.js
+++ headers.js
@@ -1,10 +1,11 @@
 let menu_els = {
-  'Accueil': "index.html",
-  'Wordle': "aideWordle.html",
-  'Anime': "animePicker.html",
-  'Portfolio': "portfolio.html",
-  'Wood Calculator': "woodCalculator.html",
-  'Jeu': "game.html"
+  'Accueil': "/index.html",
+  'Wordle': "/aideWordle.html",
+  'Anime': "/animePicker.html",
+  'Portfolio': "/portfolio.html",
+  'Wood Calculator': "/woodCalculator.html",
+  'Jeu': "/game/game.html",
+  'Jeu 2': "/game2.html"
 };
 let head_el = document.getElementById("headers");
 
@@ -13,7 +14,7 @@ dictList = (dict) => {
     let li = document.createElement('li');
     let a = document.createElement('a');
     a.href = dict[item];
-    if(document.baseURI.split('/').at(-1) === dict[item]){
+    if(document.baseURI.split('/').at(-1) === dict[item].split('/').at(-1)){
       a.className = "active";
     } else if (document.baseURI.split('/').at(-1) === "" && item === "Accueil"){
       a.className = "active";
