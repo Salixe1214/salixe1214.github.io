385604d4d636ce372d46278e608ceb14b329573b
ast-amand <alexis.st-amand@novolecs.com>
2026-04-20 09:45:28 -0400

    + default collisions

--- game2.js
+++ game2.js
@@ -23,25 +23,80 @@ function gameTick() {
 
 function physics() {
   adjustVelocity();
-  position.y += velocity.y;
-  position.x += velocity.x;
-  if(position.y > 480 - rad){
+
+  if(canMoveX()){position.x += velocity.x;};
+  if(canMoveY()){position.y += velocity.y;};
+
+  if(position.y + rad > 480){
     position.y = 480 - rad;
   };
-  if(position.y < 0 + rad){
+  if(position.y - rad < 0){
     position.y = 0 + rad;
   };
-  if(position.x < 0 + rad){
+  if(position.x - rad < 0){
     position.x = 0 + rad;
   };
-  if(position.x > 640 - rad){
+  if(position.x + rad > 640){
     position.x = 640 - rad;
   };
 };
 
+function canMoveX() {
+  if(position.y + rad > 95 && position.y - rad < 155){
+    if(position.x + rad + velocity.x > 295 && position.x - rad + velocity.x < 405){
+      return false;
+    };
+  };
+
+  if(position.y + rad > 245 && position.y - rad < 305){
+    if(position.x + rad + velocity.x > 495 && position.x - rad + velocity.x < 575){
+      return false;
+    };
+  };
+
+  return true;
+}
+
+function canMoveY() {
+  if(position.x + rad > 295 && position.x - rad < 405){
+    if(position.y + rad + velocity.y > 95 && position.y - rad + velocity.y < 155){
+      return false;
+    };
+  };
+
+  if(position.x + rad > 495 && position.x - rad < 575){
+    if(position.y + rad + velocity.y > 245 && position.y - rad + velocity.y < 305){
+      return false;
+    };
+  };
+
+  return true;
+}
+
 function draw() {
   ctx.fillStyle = "white"
   ctx.fillRect(0, 0, 640, 480);
+
+  ctx.fillStyle = "black"
+  ctx.stroke()
+  ctx.fillRect(300, 100, 100, 50);
+  ctx.beginPath();
+  ctx.moveTo(295, 95);
+  ctx.lineTo(405, 95);
+  ctx.lineTo(405, 155);
+  ctx.lineTo(295, 155);
+  ctx.lineTo(295, 95);
+  ctx.stroke();
+
+  ctx.stroke()
+  ctx.fillRect(500, 250, 70, 50);
+  ctx.beginPath();
+  ctx.moveTo(495, 245);
+  ctx.lineTo(575, 245);
+  ctx.lineTo(575, 305);
+  ctx.lineTo(495, 305);
+  ctx.lineTo(495, 245);
+  ctx.stroke();
 
   ctx.fillStyle = "black"
   ctx.beginPath();
